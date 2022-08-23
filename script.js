78bc3c504fd037128110baa13e3c2bf0c8c477c2
Basem Elsawy <besoelpop2@gmail.com>
2022-08-23 19:42:26 +0200

    last edit to the page style

---
+++ script.js
@@ -0,0 +1,8 @@
+//Calling elements from the DOM
+const itemsQty = document.querySelector('.items-quantity');
+const incrementBtn = document.querySelector('.increment');
+const decrementBtn = document.querySelector('.decrement');
+const merchTotal = document.querySelector('.merch-total');
+const orderTotal = document.querySelector('order-total');
+
+//EventListeners & Functions
