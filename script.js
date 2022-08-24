e3be05ee14d9ab2ecc1593cc31c4876fb43d9265
Basem Elsawy <besoelpop2@gmail.com>
2022-08-24 16:14:40 +0200

    javascript edit

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 //Calling elements from the DOM
-const itemsQty = document.querySelector('.items-quantity');
-const incrementBtn = document.querySelector('.increment');
-const decrementBtn = document.querySelector('.decrement');
-const merchTotal = document.querySelector('.merch-total');
-const orderTotal = document.querySelector('order-total');
+let itemsQty = document.querySelector('.items-quantity');
+let incrementBtn = document.querySelectorAll('.increment');
+let decrementBtn = document.querySelectorAll('.decrement');
+let merchTotal = document.querySelector('.merch-total');
+let orderTotal = document.querySelector('.order-total');
+let prices = document.querySelectorAll('.price');
+let productQty = document.querySelectorAll('.adding');
+let product = document.querySelectorAll('.add-remove');
+let removeBtn = document.querySelectorAll('.RemoveItm');
+// --------------------/*EventListeners & Functions */
 
-//EventListeners & Functions
+let productCalc = () => {
+  itemsQty.innerHTML = product.length;
+};
+
+productCalc();
+//------------------------------increment btn -----------
+incrementBtn.forEach((btn, idx) => {
+  btn.addEventListener('click', () => {
+    productQty[idx].innerHTML++;
+  });
+});
+
+//----------------------------decrement btn-------------
+
+decrementBtn.forEach((btn, idx) => {
+  btn.addEventListener('click', () => {
+    if (productQty[idx].innerHTML <= 1) {
+      del(product, idx);
+    } else {
+      productQty[idx].innerHTML--;
+    }
+  });
+});
+
+//--------------------DELETE FUNCTION---------------
+
+del = (arr, idx) => {
+  arr[idx].remove();
+};
+
+//--------------------REMOVE BTN --------------------
+removeBtn.forEach((btn, idx) => {
+  btn.addEventListener('click', () => {
+    del(product, idx);
+  });
+});
